531b8dcee26b514daba5ecaa7922cc6de84098d8
Shivam Ahuja <techsavythor@gmail.com>
2019-11-18 00:05:43 +0100

    first lambda expression

--- src/main/java/lamdabasics/Greeter.java
+++ src/main/java/lamdabasics/Greeter.java
@@ -9,5 +9,16 @@ public class Greeter {
         Greeter greeter = new Greeter();
         HelloWorldGreeting helloWorldGreeting = new HelloWorldGreeting();
         greeter.greet(helloWorldGreeting);
+        MyLambda myLambdaFunction = ()-> System.out.println("Hello World!");
+        MyAdd addFunction = (int a, int b)->a+b;
     }
 }
+/*
+interface should have exact same signature as of the lambda function
+ */
+interface MyLambda{
+    void foo();
+}
+interface MyAdd{
+            int add(int x, int y);
+        }
