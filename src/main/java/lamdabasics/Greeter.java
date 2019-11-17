9666bcaf1ad2311b71247425e72f9ce2ef8b1a04
Shivam Ahuja <techsavythor@gmail.com>
2019-11-18 00:07:13 +0100

    lambda expression by using the existing Greeting interface

--- src/main/java/lamdabasics/Greeter.java
+++ src/main/java/lamdabasics/Greeter.java
@@ -9,16 +9,6 @@ public class Greeter {
         Greeter greeter = new Greeter();
         HelloWorldGreeting helloWorldGreeting = new HelloWorldGreeting();
         greeter.greet(helloWorldGreeting);
-        MyLambda myLambdaFunction = ()-> System.out.println("Hello World!");
-        MyAdd addFunction = (int a, int b)->a+b;
+        Greeting myLambdaFunction = ()-> System.out.println("Hello World!");
     }
 }
-/*
-interface should have exact same signature as of the lambda function
- */
-interface MyLambda{
-    void foo();
-}
-interface MyAdd{
-            int add(int x, int y);
-        }
