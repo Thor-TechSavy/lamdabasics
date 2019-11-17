bcfdad956553a4ff1421ee44d46a3db05715b8b1
Shivam Ahuja <techsavythor@gmail.com>
2019-11-17 23:35:10 +0100

    passing the behaviour(implementation) to the greet method *polymorphism*

--- src/main/java/lamdabasics/Greeter.java
+++ src/main/java/lamdabasics/Greeter.java
@@ -2,11 +2,12 @@ package lamdabasics;
 
 public class Greeter {
 
-    public void greet(){
-        System.out.println("Hello World");
+    public void greet(Greeting greeting){
+        greeting.perform();
     }
     public static void main(String args []){
         Greeter greeter = new Greeter();
-        greeter.greet();
+        HelloWorldGreeting helloWorldGreeting = new HelloWorldGreeting();
+        greeter.greet(helloWorldGreeting);
     }
 }
---
+++ src/main/java/lamdabasics/HelloWorldGreeting.java
@@ -0,0 +1,9 @@
+package lamdabasics;
+
+import java.sql.SQLOutput;
+
+public class HelloWorldGreeting implements Greeting {
+    public void perform() {
+        System.out.println("Hello World");
+    }
+}
