9ccb58f7dde83cda88929cfb53c0ca7b1333694b
Shivam Ahuja <techsavythor@gmail.com>
2019-11-19 00:02:10 +0100

    add functional interface annotation

--- src/main/java/lamdabasics/Greeting.java
+++ src/main/java/lamdabasics/Greeting.java
@@ -2,7 +2,7 @@ package lamdabasics;
 /*
 functional interface should have only one method
  */
+@FunctionalInterface
 public interface Greeting {
-    public void perform();
- //   int add(int a , int b); //wrong
+    void perform();
 }
