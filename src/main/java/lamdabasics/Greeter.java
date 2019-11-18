e95b1007814166547cfdd61192659987498b6079
Shivam Ahuja <techsavythor@gmail.com>
2019-11-18 23:28:47 +0100

    type inference example

--- src/main/java/lamdabasics/Greeter.java
+++ src/main/java/lamdabasics/Greeter.java
@@ -4,10 +4,7 @@ package lamdabasics;
 
 public class Greeter {
 
-    public void greet(Greeting greeting){
-        greeting.perform();
-    }
-    public static void main(String args []){
+    public static void main(String[] args) {
         Greeter greeter = new Greeter();
 
         Greeting lambdaGreeting = ()-> System.out.println("Hello World");
@@ -19,5 +16,9 @@ public class Greeter {
         };
         greeter.greet(lambdaGreeting);
         greeter.greet(innerClassGreeting);
+    }
+
+    public void greet(Greeting greeting) {
+        greeting.perform();
     }
 }
---
+++ src/main/java/lamdabasics/TypeInferenceExample.java
@@ -0,0 +1,17 @@
+package lamdabasics;
+
+public class TypeInferenceExample {
+    public static void main(String[] args) {
+        //StringLengthLambda stringLengthLambda = s -> s.length();
+        //printLambda(stringLengthLambda);
+        printLambda(s -> s.length());
+    }
+
+    public static void printLambda(StringLengthLambda lengthLambda) {
+        System.out.println(lengthLambda.getLength("Hello World"));
+    }
+
+    interface StringLengthLambda {
+        int getLength(String s);
+    }
+}
