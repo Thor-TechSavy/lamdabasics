e79abf564ddc84a7dbea6a8e5db02a44d5f0f347
Shivam Ahuja <techsavythor@gmail.com>
2019-11-20 23:33:43 +0100

    unit 1 exercise and solution

---
+++ src/main/java/lamdabasics/Unit1ExerciseSolutionJava7.java
@@ -0,0 +1,44 @@
+package lamdabasics;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Unit1ExerciseSolutionJava7 {
+    public static void main(String[] args) {
+        List<Person> people = Arrays.asList(
+                new Person("Charles", "Dickens", 60),
+                new Person("Lewis", "Carroll", 42),
+                new Person("Thomas", "Carlyle", 51),
+                new Person("Charlotte", "Bronte", 45),
+                new Person("Matthew", "Arnold", 39)
+        );
+        //sort list by last name
+        Collections.sort(people, new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                return o1.getLastName().compareTo(o2.getLastName());
+            }
+        });
+        //create method to print all elements in the list
+        printAll(people);
+        //create method to print all elements that have last name beginning with C
+        printLastNameBeginsWithC(people);
+    }
+
+    private static void printLastNameBeginsWithC(List<Person> people) {
+        for (Person p : people) {
+            if (p.getLastName().startsWith("C")) {
+                System.out.println(p);
+            }
+        }
+
+    }
+
+    private static void printAll(List<Person> people) {
+        for (Person p : people) {
+            System.out.println(p);
+        }
+    }
+}
