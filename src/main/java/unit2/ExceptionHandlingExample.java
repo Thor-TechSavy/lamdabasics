fe22c4eb3666f21f52f6c1d5a90d1cf17cdae358
Shivam Ahuja <techsavythor@gmail.com>
2019-11-24 16:34:14 +0100

    exception handling using try catch in lamda expression

--- src/main/java/unit2/ExceptionHandlingExample.java
+++ src/main/java/unit2/ExceptionHandlingExample.java
@@ -6,17 +6,19 @@ public class ExceptionHandlingExample {
     public static void main(String[] args) {
         int[] someNumbers = {1, 2, 3, 4, 5};
         int key = 2;
-        process(someNumbers, key, (v, k) -> System.out.println(v / k));
+        process(someNumbers, key, (v, k) -> {
+            try {
+                System.out.println(v / k);
+            } catch (ArithmeticException e) {
+                System.out.println("Arithmetic exception happened");
+            }
+        });
 
     }
 
     private static void process(int[] someNumbers, int key, BiConsumer<Integer, Integer> biConsumer) {
         for (int i : someNumbers) {
-            try {
                 biConsumer.accept(i, key);
-            } catch (ArithmeticException e) {
-                //exception
-            }
         }
     }
 }
