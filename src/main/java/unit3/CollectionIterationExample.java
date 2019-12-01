95870578aa4b685482c82272d8f1bfd0d96923d2
Shivam Ahuja <techsavythor@gmail.com>
2019-12-01 23:11:06 +0100

    usage of forEach(Consumer<T> consumer)

---
+++ src/main/java/unit3/CollectionIterationExample.java
@@ -0,0 +1,21 @@
+package unit3;
+
+import common.Person;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CollectionIterationExample {
+    public static void main(String[] args) {
+        List<Person> people = Arrays.asList(
+                new Person("Charles", "Dickens", 60),
+                new Person("Lewis", "Carroll", 42),
+                new Person("Thomas", "Carlyle", 51),
+                new Person("Charlotte", "Bronte", 45),
+                new Person("Matthew", "Arnold", 39)
+        );
+        people.forEach(System.out::println);//p -> System.out.println(p)
+
+
+    }
+}
