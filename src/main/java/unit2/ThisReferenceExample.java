42e647f14f01a7badfc3f4a511220ca4b1a3bfb9
Shivam Ahuja <techsavythor@gmail.com>
2019-12-01 15:34:27 +0100

    this reference behaviour with the lambda

--- src/main/java/unit2/ThisReferenceExample.java
+++ src/main/java/unit2/ThisReferenceExample.java
@@ -1,19 +1,21 @@
 package unit2;
 
 public class ThisReferenceExample {
+
     public static void main(String[] args) {
         ThisReferenceExample thisReferenceExample = new ThisReferenceExample();
-        thisReferenceExample.doProcess(10, new Process() {
-            @Override
-            public void process(int i) {
-                System.out.println(i);
-                System.out.println(this);
-            }
+        thisReferenceExample.execute();
+    }
 
-            public String toString() {
-                return "This is anonymous inner class";
-            }
+    public void execute() {
+        doProcess(10, i -> {
+            System.out.println(i);
+            System.out.println(this);
         });
+    }
+
+    public String toString() {
+        return "This is main ThisReferenceExample class instance";
     }
 
     public void doProcess(int i, Process process) {
